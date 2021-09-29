f706724526e1e1d5dc8fa9ff46bdc550d7ca4aec
wEiRdChAmP1224 <88259101+wEiRdChAmP1224@users.noreply.github.com>
2021-09-29 17:57:37 +0530

    Add files via upload

---
+++ commands/uptime/help.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+const { default_prefix } = require("./../../config.json");
+
+module.exports = {
+  name: "help",
+  description: "Shows all commands of the bot",
+  category: "uptime",
+  botPermission: [],
+  authorPermission: [],
+  ownerOnly: false,
+  run: async (client, message, args) => {
+
+
+    const commands = client.commands
+      .filter((c) => c.ownerOnly === false)
+      .map((cmd) => `**${default_prefix}${cmd.name}** - ${cmd.description}`);
+
+    const contents =
+      "**Uptimer** a free discord bot that allows you to make your projects online 24/7 just by using a single cmd.\n\n" +
+      commands.sort().join("\n");
+
+    let embed = new MessageEmbed()
+      .setTitle(" Here are my comamnds!")
+      .setDescription(contents)
+      .setColor("RANDOM")
+      .setFooter(`Prefix: "${default_prefix}"`)
+      .setThumbnail(client.user.displayAvatarURL())
+
+      .setTimestamp();
+
+    return message.channel.send(embed);
+  },
+};
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at Port: ${port}`);
+});
+
+require("dotenv").config();
+const { Client, Collection } = require("discord.js");
+const UrlsConfig = require("./database/models/UrlsConfig");
+const fetchProjects = require("./fetchProjects");
+const { timeout, disable_fetching } = require("./config.json");
+
+const client = new Client({
+  disableEveryone: true,
+});
+
+(async () => {
+  await require("./database/connect")();
+
+  let pros = await UrlsConfig.find();
+
+  client.commands = new Collection();
+  client.aliases = new Collection();
+  client.projectsSize = 0;
+  client.projects = pros.map((p) => p.projectURL);
+
+  UrlsConfig.countDocuments({}, async (err, total) => {
+    client.projectsSize = total;
+
+    ["command", "events"].forEach((handler) => {
+      require(`./handlers/${handler}`)(client);
+    });
+
+    await client.login(process.env.BOT_TOKEN);
+
+    if (!disable_fetching) fetchProjects(client.projects, client);
+  });
+})();
+
+// pinging
+setInterval(async () => {
+  UrlsConfig.countDocuments({}, (err, total) => {
+    client.projectsSize = total;
+    client.user.setActivity(`${total} Project(s)`, {
+      type: "WATCHING",
+    });
+  });
+
+  if (!disable_fetching) fetchProjects(client.projects, client);
+}, timeout);
